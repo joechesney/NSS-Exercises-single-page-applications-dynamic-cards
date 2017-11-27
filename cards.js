40cabb7fc31ec67975b5983206a2b3f4441b24af
joechesney <33613119+joechesney@users.noreply.github.com>
2017-11-26 22:23:15 -0600

    removed the stupid semi colon

--- cards.js
+++ cards.js
@@ -1,15 +1,16 @@
 // creates var for the button
 const createButton = document.getElementById("creator");
-
+const textbox1 = document.getElementById("inputBox");
 // event listener that should create a new div element, then grab the content from the textbox, then place that ocntent inside the new div, then place that new div inside of the div with id=container
-createButton.addEventListener("click", createOne(event) {
+createButton.addEventListener("click", function(ev) {
         let newCard = document.createElement("div");
-        let textbox1 = document.getElementById("inputBox");
-        let cardContent = textbox1.value; //necessary?
-        // newCard.innerHTML = cardContent //necessary?
+        let newContent = textbox1.value; 
+        let newContentNode = document.createTextNode(newContent);
+        newCard.appendChild(newContentNode);
+        // newCard.innerHTML = cardContent; //necessary?
         //---OR THIS---
-        newCard.appendChild(cardContent); 
+        // newCard.appendChild(cardContent); 
         document.getElementById("container").appendChild(newCard);
 
-    };
+    }
 );
